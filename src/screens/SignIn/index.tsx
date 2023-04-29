85139a8144f4475a885036c3d3d2d348427c6b15
Mateus8741 <mateus_sousa_tavares@hotmail.com>
2023-04-29 17:58:03 -0300

    :lipstick: feat: Styled SignIn screen

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,13 @@
 import * as S from './styles'
 
+import backgroundImg from '../../assets/background.png'
+
 export function SignIn() {
-  return <S.Container></S.Container>
+  return (
+    <S.Container source={backgroundImg}>
+      <S.Title>Ignite Fleet</S.Title>
+
+      <S.Slogan>Gestão de uso de Veículos</S.Slogan>
+    </S.Container>
+  )
 }
