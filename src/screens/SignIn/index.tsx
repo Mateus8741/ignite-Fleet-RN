bb13142f312c3e4a154e2e371dab5af58cca9ab6
Mateus8741 <mateus_sousa_tavares@hotmail.com>
2023-04-30 09:01:42 -0300

    :sparkles: feat: Button Login with Google

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,6 +1,7 @@
 import * as S from './styles'
 
 import backgroundImg from '../../assets/background.png'
+import { Button } from '../../components/Button'
 
 export function SignIn() {
   return (
@@ -8,6 +9,8 @@ export function SignIn() {
       <S.Title>Ignite Fleet</S.Title>
 
       <S.Slogan>Gestão de uso de Veículos</S.Slogan>
+
+      <Button title="Entrar com google" />
     </S.Container>
   )
 }
