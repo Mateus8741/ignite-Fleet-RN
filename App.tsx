76f7cc2efe24368e47424292697b7f24c4f2d7a6
Mateus8741 <mateus_sousa_tavares@hotmail.com>
2023-04-30 11:39:58 -0300

    :heavy_plus_sign: realm db

--- App.tsx
+++ App.tsx
@@ -1,13 +1,21 @@
 /* eslint-disable camelcase */
+import { REALM_APP_ID } from '@env'
+
 import {
   Roboto_400Regular,
   Roboto_700Bold,
   useFonts,
 } from '@expo-google-fonts/roboto'
+
+import { AppProvider, UserProvider } from '@realm/react'
+
 import { StatusBar } from 'react-native'
+
 import { ThemeProvider } from 'styled-components/native'
 import { Loading } from './src/components/Loading/indes'
+import { Home } from './src/screens/Home'
 import { SignIn } from './src/screens/SignIn'
+
 import theme from './src/theme'
 
 export default function App() {
@@ -21,13 +29,17 @@ export default function App() {
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      <SignIn />
-    </ThemeProvider>
+    <AppProvider id={REALM_APP_ID}>
+      <ThemeProvider theme={theme}>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        <UserProvider fallback={SignIn}>
+          <Home />
+        </UserProvider>
+      </ThemeProvider>
+    </AppProvider>
   )
 }
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,16 +1,62 @@
+/* eslint-disable no-unused-vars */
+import * as Google from 'expo-auth-session/providers/google'
+import * as WebBrowser from 'expo-web-browser'
 import * as S from './styles'
 
 import backgroundImg from '../../assets/background.png'
 import { Button } from '../../components/Button'
 
+import { ANDROID_CLIENT_ID, IOS_CLIENT_ID } from '@env'
+import { useEffect, useState } from 'react'
+import { Alert } from 'react-native'
+
+WebBrowser.maybeCompleteAuthSession()
+
 export function SignIn() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false)
+
+  const [_, response, googleSignIn] = Google.useIdTokenAuthRequest({
+    androidClientId: ANDROID_CLIENT_ID,
+    iosClientId: IOS_CLIENT_ID,
+    scopes: ['profile', 'email'],
+  })
+
+  function handleGoogleSignIn() {
+    setIsAuthenticating(true)
+
+    googleSignIn().then(() => {
+      if (response?.type !== 'success') {
+        setIsAuthenticating(false)
+      }
+    })
+  }
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      if (response.authentication?.idToken) {
+        fetch(
+          `https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${response.authentication.idToken}`,
+        )
+          .then((response) => response.json())
+          .then(console.log)
+      } else {
+        Alert.alert('Erro ao autenticar')
+        setIsAuthenticating(false)
+      }
+    }
+  }, [response])
+
   return (
     <S.Container source={backgroundImg}>
       <S.Title>Ignite Fleet</S.Title>
 
       <S.Slogan>Gestão de uso de Veículos</S.Slogan>
 
-      <Button title="Entrar com google" />
+      <Button
+        title="Entrar com google"
+        onPress={handleGoogleSignIn}
+        isLoading={isAuthenticating}
+      />
     </S.Container>
   )
 }
